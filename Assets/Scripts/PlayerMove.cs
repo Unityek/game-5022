5b8231a2514253a158ec1e657a07ac1bc4cfd617
Unityek <untrainek@gmail.com>
2023-04-02 15:16:54 +0300

    Создал PlayerMove and PlayerLook

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float speed;
+    private Vector3 direction;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveHor = Input.GetAxis("Horizontal");
+        float moveVer = Input.GetAxis("Vertical");
+
+        direction = new Vector3(moveHor, 0, moveVer);
+        direction = Vector3.ClampMagnitude(direction, 1);
+        direction = transform.TransformDirection(direction);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + direction * speed * Time.deltaTime);
+    }
+}
